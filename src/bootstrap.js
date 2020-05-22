8a6885343af21b59cf4c8f31fe857c8c14ba500a
Daniel Karski <daniel.karski5q@gmail.com>
2020-05-22 19:46:06 +0200

    Implement facade pattern for services

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,11 +1,16 @@
 import { App } from "./app/app.js";
 import store from "./store/store.js";
+import fileService from "./services/file-service.js";
+import tagService from "./services/tag-service.js";
 
 function bootstrap() {
   window.addEventListener("DOMContentLoaded", () => {
     const application = new App(document.querySelector("#app"));
 
-    application.render();
+    Promise.all([
+      fileService.getAll(),
+      tagService.getAll(),
+    ]).then(([files, tags]) => store.update({ files, tags }));
 
     store.subscribe(() => application.render());
   });
